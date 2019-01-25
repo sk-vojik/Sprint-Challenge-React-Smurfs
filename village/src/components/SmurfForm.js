b2ce51cc8ea4373efeab36b244083ebe030c6248
Scott Vojik <scott.vojik@gmail.com>
2019-01-25 08:40:17 -0800

    adding functionality complete

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -13,13 +13,13 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
     this.setState({
       name: '',
       age: '',
       height: ''
     });
-  }
+    this.props.updateSmurfs(this.state)
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
 
 import Smurf from './Smurf';
 
@@ -20,6 +22,7 @@ class Smurfs extends Component {
             );
           })}
         </ul>
+        <Link to="/smurf-form">Add a smurf!</Link>
       </div>
     );
   }
